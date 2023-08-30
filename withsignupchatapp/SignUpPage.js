dd82420835aa9a8b30715558336ee03ce9c1e992
DharshiniSrinivasan123 <141119206+DharshiniSrinivasan123@users.noreply.github.com>
2023-08-30 03:31:55 -0700

    Signup page also included in the chatapp
    Signup page also included in the chatapp- Reactnative

---
+++ withsignupchatapp/SignUpPage.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const SignUpPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = () => {
+    // In a real app, you would typically make an API call to sign up the user
+    // This is a mock implementation for demonstration purposes
+    console.log('Sign up:', { name, email, phone, password });
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    // Simple phone number validation (just checking length)
+    return phoneNumber.length === 10;
+  };
+
+  return (
+    <View style={styles.signupContainer}>
+      <Text style={styles.header}>Sign Up</Text>
+      <View style={styles.formGroup}>
+        <Text>Name</Text>
+        <TextInput
+          style={styles.input}
+          value={name}
+          onChangeText={setName}
+        />
+      </View>
+      <View style={styles.formGroup}>
+        <Text>Email</Text>
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      <View style={styles.formGroup}>
+        <Text>Phone Number</Text>
+        <TextInput
+          style={styles.input}
+          value={phone}
+          onChangeText={setPhone}
+        />
+        {phone && !validatePhoneNumber(phone) && <Text style={styles.error}>Invalid phone number</Text>}
+      </View>
+      <View style={styles.formGroup}>
+        <Text>Password</Text>
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+        />
+        {password && password.length < 6 && <Text style={styles.error}>Password must be at least 6 characters</Text>}
+      </View>
+      <Button title="Sign Up" onPress={handleSignUp} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  signupContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  formGroup: {
+    marginBottom: 15,
+  },
+  input: {
+    width: 250,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  error: {
+    color: 'red',
+  },
+});
+
+export default SignUpPage;
